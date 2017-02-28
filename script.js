afa2272442faac8190be3147c2b75957e8bab8ce
Abubakar Usman <abusmoh@gmail.com>
2017-02-28 14:36:18 +0100

    added reset and go buttons

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
-// Define variables: trials, count = 0, array of cities
+// Define variables: trials, count = 0, array of cities, etc.
 var trials;
 var count = 0;
 var randNumber;
-var arr = [ "Kabul","Tirana","Algiers","Andorra","Luanda","Yerevan","Canberra","Vienna","Baku","Nassau" ]
+var arr = [ "Kabul","Tirana","Algiers","Andorra","Luanda","Yerevan","Canberra","Vienna","Baku","Nassau", "Lagos", "Abuja", "Kano", "Zaria", "Warri", "Minna"];
+var oldArr = [];
 var usedArr = [];
 var randCity = "";
-
-//Ramdomly pick a city from array
-randNumber = Math.floor(Math.random()*arr.length);
-randCity = arr[randNumber];
-
-// assign no of trials = cityName.length+2
-trials = randCity.length + 2;
+var kbdElement;
 var cityDOM = document.getElementById('boxes');
+var rButton = document.getElementById('rButton');
+var gButton = document.getElementById('gButton');
+
+
+
+// Assign no of trials = cityName.length+2
+trials = randCity.length + 2;
+
+function resetBoxes(){
+    for(let i=0;i<=randCity.length-1;i++){
+      cityDOM.childNodes[i].textContent = "";
+    }
+}
+
+function removeBoxes(){
+  //Remove existing boxes from the DOM boxes if any
+  while (cityDOM.hasChildNodes()) {
+    cityDOM.removeChild(cityDOM.lastChild);
+  }
+}
 
 function generateBoxes(){
-    // create boxes equal to the length of the city name and insert city name characters in each box
-    for(i=0; i<=randCity.length-1;i++){
+
+    removeBoxes();
+
+    //Ramdomly pick a city from array
+    randNumber = Math.floor(Math.random()*arr.length);
+    randCity = arr[randNumber];
+
+    // Create boxes equal to the length of the city name and insert city name characters in each box
+    for(let i=0; i<=randCity.length-1;i++){
       var box = document.createElement('div');
       box.classList.add('box');
       cityDOM.append(box);
       var kbdElement = document.createElement('kbd');
-      box.append(randCity[i], kbdElement);
+      kbdElement.innerHTML = randCity[i].toUpperCase();
+      box.append(kbdElement);
     }
-
-    return cityDOM;
 }
-
 
     // while(trials >= 0){
     //
@@ -47,9 +67,7 @@ function generateBoxes(){
         //else play 'kick' sound and reduce noOfTrials by 1
 
 window.onload = function(){
+  //let resetButton = document.getElementById('reset').addEventListener('click', resetBoxes());
   generateBoxes();
-  console.log(cityDOM);
-
-
 
 }
